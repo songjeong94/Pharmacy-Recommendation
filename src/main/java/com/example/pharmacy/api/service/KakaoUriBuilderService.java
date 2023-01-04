fe6d409c026f8d9379f9ea32cc4e87e4d5ca0b03
songjeonghyun11 <thdwjd7899@naver.com>
2023-01-04 21:40:26 +0900

    second commit(spock test)

---
+++ src/main/java/com/example/pharmacy/api/service/KakaoUriBuilderService.java
@@ -0,0 +1,25 @@
+package com.example.pharmacy.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class KakaoUriBuilderService {
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    //uri만들기
+    public URI buildUriByAddressSearch(String address) {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        uriBuilder.queryParam("query", address);
+
+        URI uri = uriBuilder.build().encode().toUri();
+        log.info("[KakaoUriBuilderService buildUriByAddressSearch ] adress: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+}
